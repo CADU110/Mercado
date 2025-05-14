a3ca045f36ebcb6709d9a5e6742eb4e16ce17856
arthurgrandao <arthur.gdemello@gmail.com>
2025-05-13 22:51:15 -0300

    add: script pra ler outputs

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd cadu/
+
+echo -e "#========================# Dataset A #========================#\n"
+
+for i in {01..20}; do
+    echo "Testando instancia: $i"
+    python3 checker.py "datasets/a/instance_00${i}.txt" "../output/a/instance_00${i}.txt"
+    echo
+done
+
+echo -e "#========================# Dataset B #========================#\n"
+
+for i in {01..15}; do
+    echo "Testando instancia: $i"
+    python3 checker.py "datasets/b/instance_00${i}.txt" "../output/b/instance_00${i}.txt"
+    echo
+done
+
+exit 0
