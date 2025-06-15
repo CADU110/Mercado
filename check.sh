857aa6fc0819c8d48f3351ac6469d58383db4796
arthurgrandao <arthur.gdemello@gmail.com>
2025-06-15 19:37:15 -0300

    Movendo projeto para a raiz do repositório

--- check.sh
+++ check.sh
@@ -6,16 +6,16 @@ echo -e "#========================# Dataset A #========================#\n"
 
 for i in {01..20}; do
     echo "Testando instancia: $i"
-    python3 checker.py "datasets/a/instance_00${i}.txt" "../output/a/instance_00${i}.txt"
+    python3 checker.py "datasets/a/instance_00${i}.txt" "./output/teste/instance_00${i}.txt"
     echo
 done
 
-echo -e "#========================# Dataset B #========================#\n"
+# echo -e "#========================# Dataset B #========================#\n"
 
-for i in {01..15}; do
-    echo "Testando instancia: $i"
-    python3 checker.py "datasets/b/instance_00${i}.txt" "../output/b/instance_00${i}.txt"
-    echo
-done
+# for i in {01..15}; do
+#     echo "Testando instancia: $i"
+#     python3 checker.py "datasets/b/instance_00${i}.txt" "../output/b/instance_00${i}.txt"
+#     echo
+# done
 
 exit 0
